cafe14885b94f624faff840868c62062a32a2920
David <david@gloony.me>
2018-11-01 22:10:28 +0100

    Indetation
    Make indent with 2 spaces on all file (before it was mixed with tab and 2 spaces)

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,43 +1,43 @@
 var keyboard = {
-	onKeyDown: function(event){
-		var chCode = ('charCode' in event) ? event.charCode : event.keyCode;
-		chCode = event.which ? event.which : event.keyCode ? event.keyCode : 0;
-		var valuetoreturn = false;
-		var OnElement = 'none';
-		var el = document.activeElement;
-		if(el&&((el.tagName.toLowerCase()=='input'&&el.type=='text')||el.tagName.toLowerCase()=='textarea')){ OnElement = el.tagName.toLowerCase(); }
-		if(chCode==27&&OnElement!='none'){
-			if((OnElement=='text'||OnElement=='input')) document.activeElement.blur();
-		}else if(OnElement=='none'){
-			if(event.ctrlKey&&event.shiftKey){
-				switch(chCode){
-					default: valuetoreturn = true;
-				}
-			}else if(event.shiftKey){
-				switch(chCode){
-					default: valuetoreturn = true;
-				}
-			}else if(event.ctrlKey){
-				switch(chCode){
-					default: valuetoreturn = true;
-				}
-			}else{
-				switch(chCode){
-					case 37: ttrss.headlines.page.previous(); break; //left
-					case 38: ttrss.article.previous(); break; //up
-					case 39: ttrss.headlines.page.next(); break; //right
-					case 40: ttrss.article.next(); break; //down
-					case 82: ttrss.refresh(); break; //r
-					default:
-						valuetoreturn = true;
-				}
-			}
-		}else{
-			valuetoreturn = true;
-		} return valuetoreturn;
-	}
+  onKeyDown: function(event){
+    var chCode = ('charCode' in event) ? event.charCode : event.keyCode;
+    chCode = event.which ? event.which : event.keyCode ? event.keyCode : 0;
+    var valuetoreturn = false;
+    var OnElement = 'none';
+    var el = document.activeElement;
+    if(el&&((el.tagName.toLowerCase()=='input'&&el.type=='text')||el.tagName.toLowerCase()=='textarea')){ OnElement = el.tagName.toLowerCase(); }
+    if(chCode==27&&OnElement!='none'){
+      if((OnElement=='text'||OnElement=='input')) document.activeElement.blur();
+    }else if(OnElement=='none'){
+      if(event.ctrlKey&&event.shiftKey){
+        switch(chCode){
+          default: valuetoreturn = true;
+        }
+      }else if(event.shiftKey){
+        switch(chCode){
+          default: valuetoreturn = true;
+        }
+      }else if(event.ctrlKey){
+        switch(chCode){
+          default: valuetoreturn = true;
+        }
+      }else{
+        switch(chCode){
+          case 37: ttrss.headlines.page.previous(); break; //left
+          case 38: ttrss.article.previous(); break; //up
+          case 39: ttrss.headlines.page.next(); break; //right
+          case 40: ttrss.article.next(); break; //down
+          case 82: ttrss.refresh(); break; //r
+          default:
+            valuetoreturn = true;
+        }
+      }
+    }else{
+      valuetoreturn = true;
+    } return valuetoreturn;
+  }
 };
 
 $(function(){
-	$('body').on('keydown', function(e){ if(keyboard.onKeyDown(e)){ return true; }else{ e.preventDefault(); return false; } });
+  $('body').on('keydown', function(e){ if(keyboard.onKeyDown(e)){ return true; }else{ e.preventDefault(); return false; } });
 });
--- js/locStore.js
+++ js/locStore.js
@@ -1,16 +1,16 @@
 var locStore = {
-	internalVar: [],
-	get: function(name){
-		if(typeof localStorage!=='undefined') return localStorage.getItem(name);
-		else if(locStore.internalVar[name]!==undefined) return locStore.internalVar[name];
-		else return null;
-	},
-	set: function(name, value){
-		if(typeof localStorage!=='undefined') return localStorage.setItem(name, value);
-		else locStore.internalVar[name] = value;
-	},
-    unset: function(name){
-      if(typeof localStorage!=='undefined') localStorage.removeItem(name);
-      else delete myArray[name];
-    }
+  internalVar: [],
+  get: function(name){
+    if(typeof localStorage!=='undefined') return localStorage.getItem(name);
+    else if(locStore.internalVar[name]!==undefined) return locStore.internalVar[name];
+    else return null;
+  },
+  set: function(name, value){
+    if(typeof localStorage!=='undefined') return localStorage.setItem(name, value);
+    else locStore.internalVar[name] = value;
+  },
+  unset: function(name){
+    if(typeof localStorage!=='undefined') localStorage.removeItem(name);
+    else delete myArray[name];
+  }
 };
